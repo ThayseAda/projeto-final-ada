968cae17ba76c3b98ecc9844fb5b7071843aaeb3
Igor Ribeiro <igor.ribeiro2334@gmail.com>
2024-09-04 15:59:42 -0300

    feat: create PetController class

---
+++ pet_controller.py
@@ -0,0 +1,13 @@
+from pet import Pet
+from pet_repository import PetRepository
+
+class PetController:
+    def __init__(self, repository):
+        self.repository = repository
+
+    def inserir_pet(self, pet):
+        self.repository.inserir(pet)
+
+    def listar_pets(self):
+        for pet in self.repository.listar_pets():
+            print(f"Nome: {pet.nome}, Idade: {pet.idade}, Peso: {pet.peso}")
